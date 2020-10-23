c4860ee10e4dae814f1ab0aad36e95e2dd9b070a
HoangLam <hoanglam271096@gmail.com>
2020-10-23 15:15:48 +0700

    Let dog eat cat

--- dog.js
+++ dog.js
@@ -2,6 +2,11 @@ var chalk = require('chalk');
 
 function Dog(name){
 	this.name = name;
+	this.stomach = [];
+}
+
+Dog.prototype.eat = function(cat){
+	this.stomach.push(cat);
 }
 
 Dog.prototype.sayHi = function(){
